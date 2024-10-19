70d56b5786bde0e8c71d0d53bc66896356b68315
Benjafto <benjaminpbs@gmail.com>
2024-10-19 15:20:50 -0300

    modificaciones de archivos autogenerados -no tener en cuenta-

--- CostaFascinosa/CostaFascinosa/obj/Debug/net8.0/CostaFascinosa.AssemblyInfo.cs
+++ CostaFascinosa/CostaFascinosa/obj/Debug/net8.0/CostaFascinosa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CostaFascinosa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f84b8a310273bcc4ca8d2e24ec8117ba1188509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3631a4dd9a6b817045cc08566a658472f8c66329")]
 [assembly: System.Reflection.AssemblyProductAttribute("CostaFascinosa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CostaFascinosa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
